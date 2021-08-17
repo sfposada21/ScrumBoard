d53ac445620080601bc77b08deed9ba4956cb52a
Stiven Posada Valcárcel <sf.posada21@gmail.com>
2021-08-17 10:25:45 -0500

    seguridad

---
+++ backend/controllers/board.js
@@ -0,0 +1,20 @@
+const Board = require("../models/board");
+
+const saveTask = async (req, res) => {
+    if(!req.body.name || !req.body.description) return res.status(400).send("Incomplete data")
+
+    const board = new Board({
+        userId : req.body.userId,
+        name : req.body.name,
+        description: req.body.description,
+        taskStatus: "to-do",    
+    })
+
+    const result = await board.save();
+    if (!result) 
+    return res.status(400).send("Failed to register task")
+    return res.status(200).send({ result })
+
+};
+
+module.exports = { saveTask };
---
+++ backend/middleware/auth.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+
+const auth = async (req, res, next)  => {
+    
+    let jwtToken = req.header("Authoriation")
+    if (!jwtToken) return res.status(400).send(" Authorization denied : no token")
+    //[Bearer, 3141mfdfsmfmsf]
+    // 0 , 1
+    jwtToken.jwtToken.split(" ")[1]
+    if (!jwtToken) return res.status(400).send(" Authorization denied : no token ")
+    // jwtToken = 3141mfdfsmfmsf
+    try {
+        const payload = await jwt.verify(jwtToken, process.env.SECRET_KEY_JWT);
+        req.user = payload;
+        next();
+
+    } catch (e) {
+        return res.status(400).send("Authorization denied : Invalid token")
+        
+    }
+
+
+
+
+
+}
